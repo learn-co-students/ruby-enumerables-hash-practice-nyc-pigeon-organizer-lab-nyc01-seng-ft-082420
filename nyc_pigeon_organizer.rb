c62e53c0da7baba9701dfbd032542e22120af2b0
Ashab Ahmed <ashabahmed7@gmail.com>
2020-07-30 01:54:54 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,18 @@
+require 'pry'
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  results = data.each_with_object({}) do |(key, value), pigeon_array|
+    
+    value.each do |next_key, names|
+      names.each do |name|
+      
+     if  !pigeon_array[name]
+       pigeon_array[name] = {}
+     end
+     if !pigeon_array[name][key]
+       pigeon_array[name][key] = []
+    end
+    pigeon_array[name][key].push(next_key.to_s)
+end
+  end
+end
 end
