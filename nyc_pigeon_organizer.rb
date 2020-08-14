e0b69eccb6260644d9d6f9777a946fc33b7971ea
Yunhao Gao <as510891328@gmail.com>
2020-08-14 20:11:04 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,22 @@
+require "pry"
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  hash = {}
+  data.each{ |key, value| 
+    value.each{ |i, value|
+      value.each{ |value| 
+        if hash[value]
+          if hash[value][key]
+            hash[value][key] << i.to_s
+          else
+            hash[value][key] = [i.to_s]
+          end
+        else
+          hash[value] = { key => [i.to_s]}
+        end
+      }
+    }
+  }
+  hash
 end
